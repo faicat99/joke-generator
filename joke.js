57d73b27e445afb6a10c123e80979fc16a286099
Fainche <faicatmck@gmail.com>
2024-08-11 13:37:55 +0100

    updated axios code

--- joke.js
+++ joke.js
@@ -1,18 +1,17 @@
 function displayJoke(response) {
 
     console.log(response.data.answer);
-
 }
 
 function generateJoke(event) {
     event.preventDefault();
     let apiKey = "bafb81c036f1dc4bfbb21532bb2ot295";
-    let prompt = "You a funny AI assistant, that tells short and sweet jokes";
-    let context = "Generate a short joke about Ireland ";
+    let prompt = "Generate a short joke about Ireland";
+    let context = "You a funny AI assistant, that tells short and sweet jokes";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     console.log("called the AI api");
-    axios.generateJoke(apiUrl).then(displayJoke);
+    axios.get(apiUrl).then(displayJoke);
 }
 
 let jokeButton = document.querySelector("#joke-button");
