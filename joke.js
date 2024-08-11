3ace28fec80f978008e4f9c9087eb187b45004fc
Fainche <faicatmck@gmail.com>
2024-08-11 14:03:44 +0100

    amended context and prompt and layout of code

--- joke.js
+++ joke.js
@@ -1,17 +1,18 @@
-function displayJoke(response) {
-
-    let jokeElement = document.querySelector("#joke")
-
-    jokeElement.innerHTML = response.data.answer;
-
-    console.log(response.data.answer);
+function displayJoke(response) { 
+    
+    new Typewriter("#joke", {
+        strings: response.data.answer,
+        autoStart: true,
+        cursor: null,
+        delay: 30,
+    });
 }
 
 function generateJoke(event) {
     event.preventDefault();
     let apiKey = "bafb81c036f1dc4bfbb21532bb2ot295";
-    let prompt = "Generate a short joke about Ireland. Please answer with basic HTML";
-    let context = "You a funny AI assistant, that tells short and sweet jokes.";
+    let prompt = "Generate a short joke about Ireland.";
+    let context = "You a funny AI assistant, that tells short and sweet jokes. The joke must be provided in basic HTML";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let jokeElement = document.querySelector("#joke");
