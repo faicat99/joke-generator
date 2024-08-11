6001f53955a47d6119cf19d42c2fb0caff1d2ef3
Fainche <faicatmck@gmail.com>
2024-08-11 13:45:39 +0100

    joke generated in console and on screen

--- joke.js
+++ joke.js
@@ -1,5 +1,9 @@
 function displayJoke(response) {
 
+    let jokeElement = document.querySelector("#joke")
+
+    jokeElement.innerHTML = response.data.answer;
+
     console.log(response.data.answer);
 }
 
@@ -7,7 +11,7 @@ function generateJoke(event) {
     event.preventDefault();
     let apiKey = "bafb81c036f1dc4bfbb21532bb2ot295";
     let prompt = "Generate a short joke about Ireland";
-    let context = "You a funny AI assistant, that tells short and sweet jokes";
+    let context = "You a funny AI assistant, that tells short and sweet jokes. Please answer with basic HTML";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     console.log("called the AI api");
