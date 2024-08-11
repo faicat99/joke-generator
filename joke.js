051e28a043b09ffdf5bf38d581a1a579efb3c764
Fainche <faicatmck@gmail.com>
2024-08-11 13:53:29 +0100

    added typewriter plugin to HTML head

--- joke.js
+++ joke.js
@@ -10,9 +10,14 @@ function displayJoke(response) {
 function generateJoke(event) {
     event.preventDefault();
     let apiKey = "bafb81c036f1dc4bfbb21532bb2ot295";
-    let prompt = "Generate a short joke about Ireland";
-    let context = "You a funny AI assistant, that tells short and sweet jokes. Please answer with basic HTML";
+    let prompt = "Generate a short joke about Ireland. Please answer with basic HTML";
+    let context = "You a funny AI assistant, that tells short and sweet jokes.";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let jokeElement = document.querySelector("#joke");
+
+    jokeElement.innerHTML = "Generating a joke for you, please wait..."
+
 
     console.log("called the AI api");
     axios.get(apiUrl).then(displayJoke);
